19bc5016d1a04bdf687e1e5c99c4fc5906e88624
Austin Langley <austinwlangley@gmail.com>
2015-06-04 15:29:26 -0700

    added controller actions, views and routes.

--- app/controllers/puzzles_controller.rb
+++ app/controllers/puzzles_controller.rb
@@ -1,22 +1,43 @@
 class PuzzlesController < ApplicationController
   def index
-  end
-
+  @puzzles = Puzzle.all       
+  end  
+  
   def show
+    @puzzle = Puzzle.find(params[:id])
   end
 
-  def new
+  def new  # part 1 of adding new bean
+    @puzzle = Puzzle.new 
+  end 
+
+  def create # part 2 of adding new bean
+    @puzzle = Puzzle.new(params.require(:puzzle).permit(:title, :image, :message)) 
+    
+    if @puzzle.save
+      redirect_to puzzles_path
+    else
+      render :new
+    end 
   end
 
-  def create
-  end
-
-  def edit
+  def edit 
+    @puzzle = Puzzle.find(params[:id])
   end
 
   def update
+    @puzzle = Puzzle.find(params[:id])
+    if @puzzle.update_attributes(params.require(:puzzle).permit(:title, :image, :message))
+    redirect_to puzzles_path
+  else
+    render :edit
+  end
   end
 
   def destroy
+    @puzzle = Puzzle.find(params[:id])
+    @puzzle.destroy
+    redirect_to puzzles_path
   end
+
 end
