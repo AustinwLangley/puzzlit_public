d35203a850ec82b0e8781ba23cb19dbe341c8418
Austin Langley <austinwlangley@gmail.com>
2015-06-04 19:49:14 -0700

    added sessions controller

--- config/routes.rb
+++ config/routes.rb
@@ -13,6 +13,9 @@ Rails.application.routes.draw do
 	get "users" => "users#index"
 	post "users" => "users#create"
 	get "signup" => "users#new"
+	get '/login'     => 'sessions#new'
+	post '/login'    => 'sessions#create'
+	delete '/logout' => 'sessions#destroy'
 
 
 
