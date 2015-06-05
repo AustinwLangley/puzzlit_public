c8e75c7cf922a483835936d1e65840fc64ccb33f
Austin Langley <austinwlangley@gmail.com>
2015-06-04 21:11:38 -0700

    added sessions

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,15 @@
 Rails.application.routes.draw do
 
+  root "sessions#new"
+
+# User routes
+	get "users" => "users#index"
+	post "users" => "users#create"
+	get "signup" => "users#new", as: :new_user
+	get '/login'     => 'sessions#new'
+	post '/login'    => 'sessions#create'
+	delete '/logout' => 'sessions#destroy'
+
 # Puzzle routes
   get "puzzles/" => "puzzles#index"
   get "puzzles/new" => "puzzles#new", as: :new_puzzle 
@@ -9,13 +19,13 @@ Rails.application.routes.draw do
   patch "puzzles/:id" => "puzzles#update" 
   delete "puzzles/:id" => "puzzzles#destroy"
 
-# User routes
-	get "users" => "users#index"
-	post "users" => "users#create"
-	get "signup" => "users#new"
-	get '/login'     => 'sessions#new'
-	post '/login'    => 'sessions#create'
-	delete '/logout' => 'sessions#destroy'
+  get "reports" => "reports#index"
+  get "reports/new" => "reports#new", as: :new_report
+  post "users/:id/reports" => "reports#create", as: :user_reports
+  get "reports/:id" => "reports#show", as: :report
+  get "reports/:id/edit" => "reports#edit", as: :edit_report
+  patch "reports/:id" => "reports#update"
+  delete "reports/:id" => "reports#destroy"
 
 
 
